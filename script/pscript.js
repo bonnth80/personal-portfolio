686ce58b307b9bde8cef3e853b44aee6c140ca00
Tom Bonner <zerosavant@yahoo.com>
2019-06-14 00:55:45 -0500

    - added animation to click hint

--- script/pscript.js
+++ script/pscript.js
@@ -5,6 +5,7 @@ let pList = document.getElementById("portlist");
 let portView = document.getElementById("entry-display");
 let splashText = document.getElementById("hometext");
 let splashTextB = document.getElementById("hometextB");
+let pviewHint = undefined;
 
 // *******************************************
 // Splash Text
@@ -31,8 +32,8 @@ splashTextB.onclick = function(){
 
 pList.innerHTML = "";
 
-// populate portfolio object list
-for (var i = 0; i < portfolio.length; i++){
+// populate left navigation pane
+for (let i = 0; i < portfolio.length; i++){
    pList.innerHTML +=
         "<li "
         + "class=\"linkList\" "
@@ -42,15 +43,38 @@ for (var i = 0; i < portfolio.length; i++){
         + "</li>\n";
 }
 
+// add click events to each item in nav pane
 for (let i = 0; i < lItems.length; i++) {
    lItems[i].addEventListener("click", function(obj){
       portView.innerHTML = stringifyPData(Number(this.getAttribute("pindex")));
-
-   })
+      pviewHint = document.getElementById("pview-clickHint");
+   })  
 }
 
+// animate click hint sign
+let animphase = 0;
+var clickHintAnim = setInterval(()=>{
+   if (pviewHint != undefined){
+      pviewHint.style.transitionDuration = "200ms";
+      var clickHintMargin = "20px";
+      switch (animphase) {
+         case 0:
+         case 2:
+            pviewHint.style.marginLeft = (Number.parseInt(clickHintMargin) + 10) + "px";
+            animphase++;
+            break;
+         default:
+            pviewHint.style.marginLeft = clickHintMargin;
+            animphase++;
+            break;
+         case 40:
+            pviewHint.style.marginLeft = clickHintMargin;
+            animphase = 0;
+      }
+   }
+},200);
 
-
+// turn portfolio data into html text
 function stringifyPData(pIndex) {
    let str = "<div>\n"
    let hasLinks = false;
@@ -109,12 +133,67 @@ function stringifyPData(pIndex) {
    return str;
 }
 
+// turn portfolio entry skill type into html display icons
 function getTypeIcon(typeString) {
+   // c++ is the only icon that's not in font-awesome, so we have to create it ourselves (svg retrieved from Icon8)
    let cppSvg =
    `<svg class="pview-svgicon" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 50 50" version="1.1" width="50px" height="50px">
-   <g id="surface1">
-   <path style=" " d="M 43.910156 12.003906 L 27.070313 2.539063 C 25.792969 1.824219 24.207031 1.824219 22.929688 2.539063 L 6.089844 12.003906 C 4.800781 12.726563 4 14.082031 4 15.535156 L 4 34.464844 C 4 35.917969 4.800781 37.273438 6.089844 37.996094 L 22.929688 47.460938 C 23.570313 47.820313 24.285156 48 25 48 C 25.714844 48 26.429688 47.820313 27.070313 47.460938 L 43.910156 37.996094 C 45.199219 37.273438 46 35.917969 46 34.464844 L 46 15.535156 C 46 14.082031 45.199219 12.726563 43.910156 12.003906 Z M 25 37 C 18.382813 37 13 31.617188 13 25 C 13 18.382813 18.382813 13 25 13 C 28.78125 13 32.273438 14.753906 34.542969 17.742188 L 30.160156 20.277344 C 28.84375 18.835938 26.972656 18 25 18 C 21.140625 18 18 21.140625 18 25 C 18 28.859375 21.140625 32 25 32 C 26.972656 32 28.84375 31.164063 30.160156 29.722656 L 34.542969 32.257813 C 32.273438 35.246094 28.78125 37 25 37 Z M 37 26 L 35 26 L 35 28 L 33 28 L 33 26 L 31 26 L 31 24 L 33 24 L 33 22 L 35 22 L 35 24 L 37 24 Z M 44 26 L 42 26 L 42 28 L 40 28 L 40 26 L 38 26 L 38 24 L 40 24 L 40 22 L 42 22 L 42 24 L 44 24 Z "/>
-   </g>
+      <g id="surface1">` +
+         "<path style=\" \" d=\"" +
+         "M 43.910156 12.003906 " +
+         "L 27.070313 2.539063 " + 
+         "C 25.792969 1.824219 24.207031 1.824219 22.929688 2.539063 " + 
+         "L 6.089844 12.003906 " + 
+         "C 4.800781 12.726563 4 14.082031 4 15.535156 " + 
+         "L 4 34.464844 " +
+         "C 4 35.917969 4.800781 37.273438 6.089844 37.996094 " +
+         "L 22.929688 47.460938  " +
+         "C 23.570313 47.820313 24.285156 48 25 48  " +
+         "C 25.714844 48 26.429688 47.820313 27.070313 47.460938  " +
+         "L 43.910156 37.996094  " +
+         "C 45.199219 37.273438 46 35.917969 46 34.464844  " +
+         "L 46 15.535156  " +
+         "C 46 14.082031 45.199219 12.726563 43.910156 12.003906  " +
+         "Z  " +
+         "M 25 37  " +
+         "C 18.382813 37 13 31.617188 13 25  " +
+         "C 13 18.382813 18.382813 13 25 13  " +
+         "C 28.78125 13 32.273438 14.753906 34.542969 17.742188  " +
+         "L 30.160156 20.277344  " +
+         "C 28.84375 18.835938 26.972656 18 25 18  " +
+         "C 21.140625 18 18 21.140625 18 25  " +
+         "C 18 28.859375 21.140625 32 25 32  " +
+         "C 26.972656 32 28.84375 31.164063 30.160156 29.722656  " +
+         "L 34.542969 32.257813  " +
+         "C 32.273438 35.246094 28.78125 37 25 37  " +
+         "Z  " +
+         "M 37 26  " +
+         "L 35 26  " +
+         "L 35 28  " +
+         "L 33 28  " +
+         "L 33 26  " +
+         "L 31 26  " +
+         "L 31 24  " +
+         "L 33 24  " +
+         "L 33 22  " +
+         "L 35 22  " +
+         "L 35 24  " +
+         "L 37 24  " +
+         "Z  " +
+         "M 44 26  " +
+         "L 42 26  " +
+         "L 42 28  " +
+         "L 40 28  " +
+         "L 40 26  " +
+         "L 38 26  " +
+         "L 38 24  " +
+         "L 40 24  " +
+         "L 40 22  " +
+         "L 42 22  " +
+         "L 42 24  " +
+         "L 44 24  " +
+         "Z \"/>" +
+      `</g>
    </svg>`
 
    if (typeof(typeString) == "string" ){
